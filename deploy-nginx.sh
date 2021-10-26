0bff9c4afcbd3f2ba5e1633f55e6461e0d9a12fe
modi <modi.tamam@gmail.com>
2021-10-26 19:22:53 +0300

    added cli

--- deploy-nginx.sh
+++ deploy-nginx.sh
@@ -1,4 +1,4 @@
 sudo yum install docker -y
-sudo systemctl start docker & sudo groupadd docker & sudo usermod -a -G docker ec2-user
+sudo systemctl start docker & sudo usermod -a -G docker ec2-user &  sudo su $USER
 docker run hello-world
 docker run -d --name docker-nginx  -p 80:80 nginx
--- ec2/create_sg.py
+++ ec2/create_sg.py
@@ -1,7 +1,7 @@
 import boto3
 
 
-def create_security_group(name,ip_addr):
+def create_security_group(name, ip_addr):
     ec2_client_ = boto3.resource('ec2')
 
     security_group = ec2_client_.create_security_group(
@@ -21,13 +21,13 @@ def create_security_group(name,ip_addr):
     )
 
     security_group.authorize_ingress(
-        CidrIp=ip_addr+'/32',
+        CidrIp=ip_addr + '/32',
         FromPort=22,
         ToPort=22,
         IpProtocol='tcp',
     )
     security_group.authorize_ingress(
-        CidrIp=ip_addr+'/32',
+        CidrIp=ip_addr + '/32',
         FromPort=80,
         ToPort=80,
         IpProtocol='tcp',
@@ -35,3 +35,7 @@ def create_security_group(name,ip_addr):
 
     print(f'Security Group {security_group.id} has been created')
     return security_group
+
+
+if __name__ == "__main__":
+    print("AAAA")
